1530f02d24de112d0c6c67f4bf2195caa2c9851b
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-15 21:30:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function makeid(l) {
   // write your code here
+	let s = "";
+  let v = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  let length = v.length;
+  for(let i=0;i<l;i++){
+      let index = Math.floor((Math.random())*length);
+      s+=v[index];
+  }
+  return s;
 }
 
 // Do not change the code below.
